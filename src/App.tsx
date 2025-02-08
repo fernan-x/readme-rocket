6aef68ca9f1e40c85a27c91bcdd52744d922ad21
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-08 16:32:12 -0500

    chore: setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ function App() {
   return (
     <>
       <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-       </div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
