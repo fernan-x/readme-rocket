3da8ef858af61ca044c42f1a5c1dfc38438579b9
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-09 18:03:12 -0500

    feat: Create base layout + install required components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { Button } from '@/components/ui/button';
 import TechnologyBadge from '@/components/common/technology-badge';
+import AppShell from './components/common/app-shell';
 
 
 function App() {
   return (
-    <div className='bg-background h-screen flex flex-col p-8 gap-4'>
-      <h1 className="text-3xl font-bold underline">
-          ReadMe Creator
-      </h1>
-      <Button>Click me</Button>
-      <TechnologyBadge name="next-js" />
-      <TechnologyBadge name="react-js" />
-      <TechnologyBadge name="tanstack-query" />
-    </div>
+    <AppShell>
+      <AppShell.Header>
+        <h1 className="text-3xl font-bold underline">
+              ReadMe Creator
+        </h1>
+      </AppShell.Header>
+
+      <AppShell.Content>
+        <AppShell.Left>
+          <p>This is the left side</p>
+        </AppShell.Left>
+        <AppShell.Right>
+          <Button>Click me</Button>
+          <TechnologyBadge name="next-js" />
+          <TechnologyBadge name="react-js" />
+          <TechnologyBadge name="tanstack-query" />
+        </AppShell.Right>
+      </AppShell.Content>
+    </AppShell>
   );
 }
 
---
+++ src/components/common/app-shell.tsx
@@ -0,0 +1,38 @@
+const AppShellSide = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col gap-8 w-full">
+      {children}
+    </div>
+  );
+};
+
+const AppShellContent = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-row gap-8 w-full h-full overflow-y-auto">
+      {children}
+    </div>
+  );
+};
+
+const AppShellHeader = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-row gap-8 w-full">
+      {children}
+    </div>
+  );
+};
+
+const AppShell = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='bg-background h-screen flex flex-col p-8 gap-8'>
+      {children}
+    </div>
+  );
+};
+
+AppShell.Header = AppShellHeader;
+AppShell.Content = AppShellContent;
+AppShell.Left = AppShellSide;
+AppShell.Right = AppShellSide;
+
+export default AppShell;
