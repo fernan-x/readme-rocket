86216c67ac95a6ee6d162b62d35f8cc4ea532e17
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-12 22:31:01 -0500

    feat: add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
+import { Rocket } from 'lucide-react';
 import AppShell from '@/components/common/app-shell';
 import Editor from '@/components/editor/editor';
-import { Rocket } from 'lucide-react';
+import { ThemeProvider } from '@/contexts/theme-provider';
 import useReadMe from './hooks/useReadMe';
 import Preview from './components/editor/preview';
+import { ThemeToggle } from './components/common/theme-toggle';
 
 
 function App() {
     const { document, setDocument } = useReadMe();
 
     return (
-        <AppShell>
-            <AppShell.Header>
-                <h1 className="text-3xl font-bold flex flex-row items-center gap-2">
-                    <Rocket color='#7c3aed' size={36} />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <AppShell>
+                <AppShell.Header>
+                    <h1 className="text-3xl font-bold flex flex-row items-center gap-2 flex-1">
+                        <Rocket color='#7c3aed' size={36} />
                     ReadMe Rocket
-                </h1>
-            </AppShell.Header>
+                    </h1>
+                    <ThemeToggle />
+                </AppShell.Header>
 
-            <AppShell.Content>
-                <AppShell.Left>
-                    <Editor
-                        document={document}
-                        setDocument={setDocument}
-                    />
-                </AppShell.Left>
-                <AppShell.Right noGap>
-                    <Preview document={document} />
-                </AppShell.Right>
-            </AppShell.Content>
-        </AppShell>
+                <AppShell.Content>
+                    <AppShell.Left>
+                        <Editor
+                            document={document}
+                            setDocument={setDocument}
+                        />
+                    </AppShell.Left>
+                    <AppShell.Right noGap>
+                        <Preview document={document} />
+                    </AppShell.Right>
+                </AppShell.Content>
+            </AppShell>
+        </ThemeProvider>
     );
 }
 
